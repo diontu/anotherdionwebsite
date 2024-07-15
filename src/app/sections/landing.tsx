6e2c5ae2e33c93cce15fef9490e2cc3624843f95
diontugit <diontugit@gmail.com>
2024-07-15 00:13:38 -0400

    minor: add primary cta buttons

--- src/app/sections/landing.tsx
+++ src/app/sections/landing.tsx
@@ -1,20 +1,24 @@
 import Heading from "@/components/heading";
 import PageSection from "@/components/page-section";
 import Subheading from "@/components/subheading";
+import { Button } from "@/shadcn-components/ui/button";
 
 export default function Landing() {
   return (
-    <PageSection className="flex justify-between items-center mt-10">
+    <PageSection className="flex justify-around items-center mt-10">
       <div className="max-w-[500px]">
         <Heading size="lg">Hi! I'm Dion</Heading>
         <Subheading>
           Website wizard. Frontend + full-stack developer. Father of a cat.
         </Subheading>
-        <div className="pt-20">
+        <div className="pt-[120px]">
           <Subheading>
             I can make custom websites for you, help with your dream web app or
             help create automations for you.
           </Subheading>
+        </div>
+        <div className="pt-5">
+          <Button>Get in touch</Button>
         </div>
       </div>
       <div>Image</div>
