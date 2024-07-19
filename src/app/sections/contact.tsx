1d4c531bb4058b7561105ddd1c86f4161c5dcf69
diontugit <diontugit@gmail.com>
2024-07-18 23:51:21 -0400

    minor: add elements to the contact form

--- src/app/sections/contact.tsx
+++ src/app/sections/contact.tsx
@@ -3,6 +3,10 @@ import PageSection from "@/components/page-section";
 import Subheading from "@/components/subheading";
 import Mark from "@/components/text-style/mark";
 import Highlight from "@/components/text-style/highlight";
+import { Card, CardContent } from "@/shadcn-components/ui/card";
+import { Input } from "@/shadcn-components/ui/input";
+import { Textarea } from "@/shadcn-components/ui/textarea";
+import { Button } from "@/shadcn-components/ui/button";
 
 export default function Contact() {
   return (
@@ -16,7 +20,16 @@ export default function Contact() {
           a message and I will get back to you as soon as possible.
         </Subheading>
       </div>
-      <div>Form</div>
+      <div className="mt-10 max-w-[500px]">
+        <Card>
+          <CardContent className="py-5 flex flex-col gap-5">
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <Textarea placeholder="Message" />
+            <Button>Submit</Button>
+          </CardContent>
+        </Card>
+      </div>
     </PageSection>
   );
 }
