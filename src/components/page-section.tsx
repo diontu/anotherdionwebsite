2ef9667a96c447c1684a6f19a1087d8b7ca1628e
diontugit <diontugit@gmail.com>
2024-08-04 14:16:42 -0400

    minor: using cva for variants in dynamic props

--- src/components/page-section.tsx
+++ src/components/page-section.tsx
@@ -1,3 +1,4 @@
+import { cva } from "class-variance-authority";
 import "./page-section.css";
 import { cn } from "@/shadcn-components/lib/utils";
 
@@ -12,15 +13,24 @@ type PageSectionProps = {
   children?: React.ReactNode;
 };
 
+const PageSectionClasses = cva(
+  "w-full relative max-w-full py-0 px-[min(10%,170px)] overflow-hidden",
+  {
+    variants: {
+      fitContent: {
+        true: "page-section-content",
+        false: "page-section-page",
+      },
+    },
+    defaultVariants: {
+      fitContent: false,
+    },
+  }
+);
+
 const PageSection = ({ fitContent, className, children }: PageSectionProps) => {
   return (
-    <div
-      className={cn(
-        `w-full ${
-          fitContent ? "page-section-content" : "page-section-page"
-        } relative max-w-full py-0 px-[min(10%,170px)] overflow-hidden ${className}`
-      )}
-    >
+    <div className={cn(PageSectionClasses({ fitContent }), className)}>
       {children}
     </div>
   );
